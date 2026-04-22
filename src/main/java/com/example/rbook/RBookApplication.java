90260d61ebfa32f1e14889fa48d67c7938a11a85
松本 陸 <riku_matsumoto1@R-ICTMac-WMLWPXMWLG-EB14.local>
2026-04-22 15:15:12 +0900

    first commit

---
+++ src/main/java/com/example/rbook/RBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.rbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rbook/RBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
